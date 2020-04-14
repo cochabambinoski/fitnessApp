5f46d0fd8f7e559dd1f637806bc21c4f0e926572
Feber <cochabambinoski@gmail.com>
2020-04-13 20:41:03 -0400

    first commit

---
+++ src/components/Card.js
@@ -0,0 +1,22 @@
+import React  from 'react'
+import exerciseImg from '../images/exercise.png'
+
+class Card extends React.Component {
+    render(){
+        return (
+            <div>
+                <div>
+                    <img src={exerciseImg}/>
+                </div>
+                <div>
+                    <h1>Technique Guides</h1>
+                    <p>
+                        Learn amazing street workout and calistenia
+                    </p>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Card
--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// const element = document.createElement('h1')
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// element.innerText = "Hey React"
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// const container = document.getElementById('root')
+
+// container.appendChild(element)
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Card from './components/Card'
+const container = document.getElementById('root')
+
+// ReactDOM.render(____QUE____,_____DONDE____)
+
+ReactDOM.render(<Card />, container)
